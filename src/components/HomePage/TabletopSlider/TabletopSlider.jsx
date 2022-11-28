f8cde8949d7d6b521db73137d178e974a50a93e7
ArkadiusKuna <kuna.arek@gmail.com>
2022-11-28 08:43:26 +0100

    tabletop desktop images added, breakpoints for correct switching between images

--- src/components/HomePage/TabletopSlider/TabletopSlider.jsx
+++ src/components/HomePage/TabletopSlider/TabletopSlider.jsx
@@ -6,18 +6,23 @@ import TabletopSliderTextSlide from 'components/HomePage/TabletopSlider/Tabletop
 
 import ksiegaStraznikaMobile from 'assets/tabletop/mobile/cthulhu-ksiega-straznika-280.webp';
 import ksiegaStraznikaTablet from 'assets/tabletop/mobile/cthulhu-ksiega-straznika-580.webp';
+import ksiegaStraznikaDesktop from 'assets/tabletop/desktop/cthulhu-ksiega-straznika-desktop.webp';
 import ksiegaStraznikaFallback from 'assets/tabletop/mobile/cthulhu-ksiega-straznika-580.jpeg';
 import zewCthulhuMobile from 'assets/tabletop/mobile/cthulhu-uslysz-zew-cthulhu-280.webp';
 import zewCthulhuTablet from 'assets/tabletop/mobile/cthulhu-uslysz-zew-cthulhu-580.webp';
+import zewCthulhuDesktop from 'assets/tabletop/desktop/cthulhu-uslysz-zew-cthulhu-desktop.webp';
 import zewCthulhuFallback from 'assets/tabletop/mobile/cthulhu-uslysz-zew-cthulhu-580.png';
 import nutkaMobile from 'assets/tabletop/mobile/nutka-w-nutke-exe-280.webp';
 import nutkaTablet from 'assets/tabletop/mobile/nutka-w-nutke-exe-580.webp';
+import nutkaDesktop from 'assets/tabletop/desktop/nutka-w-nutke-exe-desktop.webp';
 import nutkaFallback from 'assets/tabletop/mobile/nutka-w-nutke-exe-580.png';
 import fantastykaMobile from 'assets/tabletop/mobile/fantastyka-280.webp';
 import fantastykaTablet from 'assets/tabletop/mobile/fantastyka-580.webp';
+import fantastykaDesktop from 'assets/tabletop/desktop/fantastyka-desktop.webp';
 import fantastykaFallback from 'assets/tabletop/mobile/fantastyka-580.jpeg';
 import sibirPunkMobile from 'assets/tabletop/mobile/sibir-punk-280.webp';
 import sibirPunkTablet from 'assets/tabletop/mobile/sibir-punk-580.webp';
+import sibirPunkDesktop from 'assets/tabletop/desktop/sibir-punk-desktop.webp';
 import sibirPunkFallback from 'assets/tabletop/mobile/sibir-punk-580.jpg';
 
 import 'swiper/css';
@@ -39,41 +44,41 @@ const TabletopSlider = () => {
       >
         <SwiperSlide className='flex items-center'>
           <TabletopSliderImageSlide
-            srcSet={`${ksiegaStraznikaMobile} 400w, ${ksiegaStraznikaTablet} 800w`}
+            srcSet={`${ksiegaStraznikaMobile} 400w, ${ksiegaStraznikaTablet} 800w, ${ksiegaStraznikaDesktop} 1200w`}
             src={ksiegaStraznikaFallback}
-            sizes='(max-width: 639px) 200px, 310px'
+            sizes='(max-width: 639px) 200px, (max-width: 1279px) 310px, (min-width: 1280px) 100vw'
             alt='Usłysz Zew Cthulhu'
           />
         </SwiperSlide>
         <SwiperSlide className='flex items-center'>
           <TabletopSliderImageSlide
-            srcSet={`${zewCthulhuMobile} 400w, ${zewCthulhuTablet} 800w`}
+            srcSet={`${zewCthulhuMobile} 400w, ${zewCthulhuTablet} 800w, ${zewCthulhuDesktop} 1200w`}
             src={zewCthulhuFallback}
-            sizes='(max-width: 639px) 200px, 310px'
+            sizes='(max-width: 639px) 200px, (max-width: 1279px) 310px, (min-width: 1280px) 100vw'
             alt='Nutka W Nutkę.EXE'
           />
         </SwiperSlide>
         <SwiperSlide className='flex items-center'>
           <TabletopSliderImageSlide
-            srcSet={`${nutkaMobile} 400w, ${nutkaTablet} 800w`}
+            srcSet={`${nutkaMobile} 400w, ${nutkaTablet} 800w, ${nutkaDesktop} 1200w`}
             src={nutkaFallback}
-            sizes='(max-width: 639px) 200px, 310px'
+            sizes='(max-width: 639px) 200px, (max-width: 1279px) 310px, (min-width: 1280px) 100vw'
             alt='Zgrozy Gotyckie'
           />
         </SwiperSlide>
         <SwiperSlide className='flex items-center'>
           <TabletopSliderImageSlide
-            srcSet={`${fantastykaMobile} 400w, ${fantastykaTablet} 800w`}
+            srcSet={`${fantastykaMobile} 400w, ${fantastykaTablet} 800w, ${fantastykaDesktop} 1200w`}
             src={fantastykaFallback}
-            sizes='(max-width: 639px) 200px, 310px'
+            sizes='(max-width: 639px) 200px, (max-width: 1279px) 310px, (min-width: 1280px) 100vw'
             alt='Aż Po Zęby'
           />
         </SwiperSlide>
         <SwiperSlide className='flex items-center'>
           <TabletopSliderImageSlide
-            srcSet={`${sibirPunkMobile} 400w, ${sibirPunkTablet} 800w`}
+            srcSet={`${sibirPunkMobile} 400w, ${sibirPunkTablet} 800w, ${sibirPunkDesktop} 1200w`}
             src={sibirPunkFallback}
-            sizes='(max-width: 639px) 200px, 310px'
+            sizes='(max-width: 639px) 200px, (max-width: 1279px) 310px, (min-width: 1280px) 100vw'
             alt='Sibir Punk'
           />
         </SwiperSlide>
--- src/layout/Box/Box.jsx
+++ src/layout/Box/Box.jsx
@@ -1,10 +1,10 @@
 const variantMap = {
   def: 'w-full p-2 text-center font-medium border-b-10 border-black space-y-4 bg-orange-400 sm:p-4 sm:space-y-6 md:space-y-4 lg:p-6 lg:space-y-6 xl:p-2 xl:space-y-2',
-  logo: 'flex justify-center !bg-gray-400 items-center xl:col-span-2 xl:row-span-2 xl:order-0',
+  logo: 'flex justify-center !bg-gray-400 items-center sm:border-r-10 xl:col-span-2 xl:row-span-2 xl:order-0 xl:border-r-0',
   image:
     '!p-0 flex flex-col row-span-2 sm:col-span-2 sm:row-span-2 sm:-order-1 xl:col-span-4 xl:row-span-3 xl:border-l-10 xl:order-2',
   about:
-    'flex flex-col justify-center items-center lg:!space-y-12 xl:!p-0 xl:!space-y-1 xl:col-span-2 xl:row-span-1 xl:order-3',
+    'flex flex-col justify-center items-center lg:!space-y-12 xl:!p-0 xl:!space-y-1 xl:col-span-2 xl:row-span-1 xl:order-3 xl:border-t-0',
   videoGames:
     '!p-0 sm:col-span-2 xl:col-span-4 xl:row-span-2 xl:border-b-0 xl:flex xl:order-4',
   workshops:
