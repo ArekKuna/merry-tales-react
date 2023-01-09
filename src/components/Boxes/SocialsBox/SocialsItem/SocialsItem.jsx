2a3af84756a8b64202b3ef123cd0a2daa5398eb3
ArkadiusKuna <kuna.arek@gmail.com>
2023-01-10 00:28:40 +0100

    fix: border color in socials component

--- src/components/Boxes/SocialsBox/SocialsItem/SocialsItem.jsx
+++ src/components/Boxes/SocialsBox/SocialsItem/SocialsItem.jsx
@@ -1,11 +1,11 @@
 const variantMap = {
-  def: 'py-2 w-1/4 bg-black border-black sm:w-full sm:h-full flex items-center justify-center',
+  def: 'py-2 w-1/4 bg-black border-white sm:w-full sm:h-full flex items-center justify-center',
   facebookHome: 'border-r-2 border-ghostWhite sm:border-b-2',
   linkedInHome: 'border-r-2 border-ghostWHite sm:border-r-0 sm:border-b-2',
   youTubeHome: 'border-r-2 border-ghostWhite',
-  facebook: 'border-r-2 bg-white xl:border-r-2',
-  linkedin: 'border-r-2 bg-white',
-  youtube: 'border-r-2 bg-white',
+  facebook: 'border-r-2 !border-black bg-white  xl:border-r-2',
+  linkedin: 'border-r-2 !border-black bg-white',
+  youtube: 'border-r-2 !border-black bg-white',
   email: 'bg-white',
 };
 
