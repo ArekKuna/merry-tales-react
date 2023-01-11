408812c1b61cd1343af7d2537b347899682f28b0
ArkadiusKuna <kuna.arek@gmail.com>
2023-01-11 22:21:28 +0100

    change: tabletopParagraph component refactor

--- src/components/TabletopCardsSlider/TabletopParagraph/TabletopParagraph.jsx
+++ src/components/TabletopCardsSlider/TabletopParagraph/TabletopParagraph.jsx
@@ -1,9 +1,25 @@
 import ReactMarkdown from 'react-markdown';
 
-const TabletopParagraph = ({ markdown }) => {
+const variantMap = {
+  cthulhu: `Tabletop RPG games are sort of my childish love, but honestly I
+            never give them up. I’ve been working as a translator (****Core Book****
+            &amp; ****Alone Against the Dark****) and scenario writer for the Polish 7th
+            ed. ****Call of Cthulhu**** (additional campaign books). Lately I’ve been
+            involved in the group of authors creating the project ****Zgrozy-Call of
+            Cthulhu RPG Supplements****.`,
+  otherTabletops: `My scenario for ****Blades in the Dark**** was published in ****Nowa Fantastyka****
+            magazine. I also had the pleasure to write supplements for Polish
+            5th ed. ****Vampire the Masquerade****, ****Sibir Punk**** and ****Nibiru RPG****. My
+            personal favourite though is the scenario for ****Tales from the Loop****
+            that I wrote in cooperation with ****Majka Jeżowska****.`,
+};
+
+const TabletopParagraph = ({ variant }) => {
+  const variantMarkdown = variantMap[variant];
+
   return (
     <ReactMarkdown className='px-6 mb-14 text-base text-center text-white sm:max-w-[75%] md:max-w-[60%] xl:mb-0 xl:max-w-[40%]'>
-      {markdown}
+      {`${variantMarkdown}`}
     </ReactMarkdown>
   );
 };
--- src/pages/TabletopGames.jsx
+++ src/pages/TabletopGames.jsx
@@ -9,29 +9,16 @@ import { cthulhuImages } from 'data/cthulhuImages';
 import { otherTabletops } from 'data/otherTabletops';
 
 const TabletopGames = () => {
-  const CthulhuMarkdown = `Tabletop RPG games are sort of my childish love, but honestly I
-            never give them up. I’ve been working as a translator (****Core Book****
-            &amp; ****Alone Against the Dark****) and scenario writer for the Polish 7th
-            ed. ****Call of Cthulhu**** (additional campaign books). Lately I’ve been
-            involved in the group of authors creating the project ****Zgrozy-Call of
-            Cthulhu RPG Supplements****.`;
-
-  const otherTabletopsMarkdown = `My scenario for ****Blades in the Dark**** was published in ****Nowa Fantastyka****
-            magazine. I also had the pleasure to write supplements for Polish
-            5th ed. ****Vampire the Masquerade****, ****Sibir Punk**** and ****Nibiru RPG****. My
-            personal favourite though is the scenario for ****Tales from the Loop****
-            that I wrote in cooperation with ****Majka Jeżowska****.`;
-
   return (
     <GlobalContainer variant='tabletop'>
       <PageTitle text='Tabletop RPG Games' />
       <div className='w-full z-10 max-w-screen-2xl flex flex-col xl:flex-row'>
         <CardsSliderBox>
-          <TabletopParagraph markdown={CthulhuMarkdown} />
+          <TabletopParagraph variant='cthulhu' />
           <TableTopCardsSlider images={cthulhuImages} />
         </CardsSliderBox>
         <CardsSliderBox>
-          <TabletopParagraph markdown={otherTabletopsMarkdown} />
+          <TabletopParagraph variant='otherTabletops' />
           <TableTopCardsSlider images={otherTabletops} />
         </CardsSliderBox>
       </div>
