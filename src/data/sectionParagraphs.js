2b6804ddbfa107c7cf34655449d57a3f89ebcdd9
ArkadiusKuna <kuna.arek@gmail.com>
2023-01-27 13:06:01 +0100

    fix: justify elements fix on 2xl screens for tabletop games page

--- src/data/sectionParagraphs.js
+++ src/data/sectionParagraphs.js
@@ -11,11 +11,13 @@ export const sectionParagraphs = {
       and VO recording.`,
   },
   cthulhu: {
-    className: 'sm:max-w-[75%] md:max-w-[60%] xl:mb-0 xl:p-0 xl:max-w-[40%]',
+    className:
+      'mx-0 sm:max-w-[75%] md:max-w-[60%] xl:mb-0 xl:p-0 xl:max-w-[40%]',
     text: `Tabletop RPG games are sort of my childish love, but honestly I never gave them up. I’ve been working as a translator (****core book**** & ****Alone Against the Dark gamebook****) and scenario writer for the Polish 7th ed. ****Call of Cthulhu**** (additional campaign books). Lately I’ve been involved in the group of authors creating the project ****Zgrozy-Call**** of ****Cthulhu RPG Supplements****.`,
   },
   otherTabletops: {
-    className: 'sm:max-w-[75%] md:max-w-[60%] xl:mb-0 xl:p-0 xl:max-w-[40%]',
+    className:
+      'mx-0 sm:max-w-[75%] md:max-w-[60%] xl:mb-0 xl:p-0 xl:max-w-[40%]',
     text: `My scenario for ****Blades in the Dark**** was published in ****Nowa Fantastyka**** magazine. I also had the pleasure to write supplements for Polish 5th ed. ****Vampire the Masquerade****, ****Sibir Punk**** and ****Nibiru RPG****. My personal favourite though is the scenario for ****Tales from the Loop**** that I wrote in cooperation with ****Majka Jeżowska****.`,
   },
   workshops: {
