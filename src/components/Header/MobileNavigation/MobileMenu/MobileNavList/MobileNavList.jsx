fef29ee457cb7ed8e5e01b34ec521abc438647fb
ArkadiusKuna <kuna.arek@gmail.com>
2023-01-30 00:22:10 +0100

    fix: vercel build error fix

--- src/components/Header/MobileNavigation/MobileMenu/MobileNavList/MobileNavList.jsx
+++ src/components/Header/MobileNavigation/MobileMenu/MobileNavList/MobileNavList.jsx
@@ -1,6 +1,6 @@
 import MobileNavItem from 'components/Header/MobileNavigation/MobileMenu/MobileNavItem/MobileNavItem';
 
-import { mobileMenuConfig } from 'data/mobileMenuConfig';
+import { mobileMenuConfig } from 'data/MobileMenuConfig';
 
 const variantMap = {
   def: 'flex flex-col items-center opacity-0 transition-all duration-500',
