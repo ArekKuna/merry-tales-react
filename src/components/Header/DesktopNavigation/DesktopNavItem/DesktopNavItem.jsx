2175345f5426074ab93274df0867c49e65c683c6
ArkadiusKuna <kuna.arek@gmail.com>
2023-01-30 13:54:59 +0100

    fix: active navlink color fix

--- src/components/Header/DesktopNavigation/DesktopNavItem/DesktopNavItem.jsx
+++ src/components/Header/DesktopNavigation/DesktopNavItem/DesktopNavItem.jsx
@@ -1,11 +1,18 @@
 import { NavLink } from 'react-router-dom';
 
 const DesktopNavItem = ({ link }) => {
+  const activeStyles = {
+    textDecoration: 'underline',
+    textUnderlineOffset: '6px',
+    color: 'rgb(234 88 12)',
+  };
+
   return (
     <li>
       <NavLink
         className='text-lg text-ghostWhite font-bold uppercase lg:text-2xl'
         to={link.to}
+        style={({ isActive }) => (isActive ? activeStyles : undefined)}
       >
         {link.text}
       </NavLink>
@@ -14,3 +21,4 @@ const DesktopNavItem = ({ link }) => {
 };
 
 export default DesktopNavItem;
+// 'text-lg text-ghostWhite font-bold uppercase lg:text-2xl'
